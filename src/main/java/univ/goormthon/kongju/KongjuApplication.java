b5f2185c052a0413c7e15950d3be4105faa39844
Jinseong <hdss8234@gmail.com>
2024-11-18 21:07:15 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/univ/goormthon/kongju/KongjuApplication.java
@@ -0,0 +1,13 @@
+package univ.goormthon.kongju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KongjuApplication.class, args);
+    }
+
+}
