784e211ca06422deaf89f13a6a6b336e619ac436
Jinseong <hdss8234@gmail.com>
2024-11-21 02:07:34 +0900

    feat: 불필요 테이블 삭제

--- src/main/java/univ/goormthon/kongju/domain/parking/repository/LandCertificationRepository.java
+++
@@ -1,9 +0,0 @@
-package univ.goormthon.kongju.domain.parking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import univ.goormthon.kongju.domain.parking.entity.LandCertification;
-
-@Repository
-public interface LandCertificationRepository extends JpaRepository<LandCertification, Long> {
-}
