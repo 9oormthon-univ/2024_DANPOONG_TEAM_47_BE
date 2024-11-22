24b81e501db4c62019d4edb9f6e909116ac1c323
Jinseong <hdss8234@gmail.com>
2024-11-23 03:15:58 +0900

    fix: 코드 오류 수정
    의존성 오주입 수정

--- src/main/java/univ/goormthon/kongju/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/univ/goormthon/kongju/domain/reservation/repository/ReservationRepository.java
@@ -2,7 +2,8 @@ package univ.goormthon.kongju.domain.reservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import univ.goormthon.kongju.domain.reservation.entity.Reservation;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Repository, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
