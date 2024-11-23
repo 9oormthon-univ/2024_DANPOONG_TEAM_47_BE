60705f1b8dd03d9fcf608dff65a7090a003b7523
Jinseong <hdss8234@gmail.com>
2024-11-23 20:54:45 +0900

    feat: 카카오 로그인 수정
    resolve: #3

--- src/main/java/univ/goormthon/kongju/global/auth/kakao/controller/KakaoAuthController.java
+++ src/main/java/univ/goormthon/kongju/global/auth/kakao/controller/KakaoAuthController.java
@@ -57,8 +57,8 @@ public class KakaoAuthController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "302", description = "메인으로 리다이렉트")
     })
-    @GetMapping("/auth/kakao/callback")
-    public ResponseEntity<?> kakaoCallback(@RequestParam String code, HttpSession session) {
+    @PostMapping("/auth/token")
+    public ResponseEntity<?> kakaoCallback(@RequestBody String code, HttpSession session) {
         KakaoToken accessToken = kakaoAuthService.getAccessToken(code);
         log.info("accessToken: {}", accessToken.accessToken());
 
@@ -71,8 +71,8 @@ public class KakaoAuthController {
 
         session.setAttribute("member", member);
         // 메인으로 redirect
-        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(frontendDomain)).build();
-        // return ResponseEntity.ok(session.getAttribute("member"));
+        // return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(frontendDomain)).build();
+        return ResponseEntity.ok(session.getAttribute("member"));
     }
 
     // 로그아웃
