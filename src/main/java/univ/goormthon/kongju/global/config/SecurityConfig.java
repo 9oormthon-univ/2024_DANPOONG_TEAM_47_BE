513d88a8f92372913a37db90dbc0e945189b4a88
Jinseong <hdss8234@gmail.com>
2024-12-30 22:50:08 +0900

    feat: SecurityConfig 작성
    resolve : #27

---
+++ src/main/java/univ/goormthon/kongju/global/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package univ.goormthon.kongju.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/api/kongju/member/profile").authenticated()
+                    .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
