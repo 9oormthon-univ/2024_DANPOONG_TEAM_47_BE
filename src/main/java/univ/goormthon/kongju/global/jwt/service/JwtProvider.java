8c94e450f7b3bbcde1d77df95fc1cfa267727f75
Jinseong <hdss8234@gmail.com>
2025-01-08 23:33:26 +0900

    feat: JwtProvider 구현
    - 토큰 생성, 조회, 검증 구현
    
    resolve : #27

--- src/main/java/univ/goormthon/kongju/global/jwt/service/JwtProvider.java
+++ src/main/java/univ/goormthon/kongju/global/jwt/service/JwtProvider.java
@@ -1,11 +1,13 @@
-package univ.goormthon.kongju.global.jwt.provider;
+package univ.goormthon.kongju.global.jwt.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Collections;
 import java.util.Date;
 
 @Component
@@ -26,13 +28,28 @@ public class JwtProvider {
     }
 
     public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public boolean validateToken(String token) {
+        try {
+            Claims claims = getClaims(token);
+            Date expiration = claims.getExpiration();
+            return expiration.after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public Authentication getAuthentication(String token) {
-    }
+        Claims claims = getClaims(token);
+        String email = claims.getSubject();
 
+        return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+    }
 
 }
