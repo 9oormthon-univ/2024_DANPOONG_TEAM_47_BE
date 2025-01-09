dddd69619abb84448490714d9a6a11654f4dea52
Jinseong <hdss8234@gmail.com>
2025-01-09 22:17:15 +0900

    feat: Security 구성 설정 변경
    - success handler 추가
    
    resolve : #27

--- src/main/java/univ/goormthon/kongju/global/config/SecurityConfig.java
+++ src/main/java/univ/goormthon/kongju/global/config/SecurityConfig.java
@@ -6,15 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import univ.goormthon.kongju.global.jwt.filter.JwtAuthenticationFilter;
+import univ.goormthon.kongju.global.jwt.handler.CustomAuthenticationSuccessHandler;
 import univ.goormthon.kongju.global.oauth2.CustomOAuth2UserService;
 import univ.goormthon.kongju.global.oauth2.CustomRequestEntityConverter;
+import univ.goormthon.kongju.global.oauth2.filter.CustomAuthorizationRequestRedirectFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,25 +24,25 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final JwtAuthenticationFilter jwtVerifyFilter;
+    private final CustomAuthorizationRequestRedirectFilter customAuthorizationEndpointFilter;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // 브라우저 환경이 아니므로 CSRF 보호 기능 비활성화
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/h2-console/**","/swagger-ui.html","/swagger-ui/**", "/api/kongju/oauth2/**", "/api/kongju/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/favicon.ico", "/h2-console/**","/swagger-ui.html","/swagger-ui/**", "/api/kongju/oauth2/**", "/api/kongju/auth/**").permitAll()
+                        .anyRequest().authenticated())
                 .headers(headers -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(customOAuth2UserService))
-                        .defaultSuccessUrl("/api/kongju/oauth2/success", true)
-                        .tokenEndpoint(token -> token
-                                .accessTokenResponseClient(accessTokenResponseClient())))
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .successHandler(customAuthenticationSuccessHandler))
+//                        .tokenEndpoint(token -> token
+//                                .accessTokenResponseClient(accessTokenResponseClient())))
+                .addFilterBefore(customAuthorizationEndpointFilter, OAuth2AuthorizationRequestRedirectFilter.class)
                 .addFilterBefore(jwtVerifyFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
