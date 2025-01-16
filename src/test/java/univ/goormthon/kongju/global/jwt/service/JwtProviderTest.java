3d6e9d6398460d26bc4962a02df9eb2b55512c1c
Jinseong <hdss8234@gmail.com>
2025-01-17 02:31:31 +0900

    test: 유효한 토큰 생성
    resolve: #30

--- src/test/java/univ/goormthon/kongju/global/jwt/service/JwtProviderTest.java
+++ src/test/java/univ/goormthon/kongju/global/jwt/service/JwtProviderTest.java
@@ -1,12 +1,16 @@
 package univ.goormthon.kongju.global.jwt.service;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import univ.goormthon.kongju.global.jwt.dto.request.TokenRequest;
+import univ.goormthon.kongju.global.jwt.dto.response.TokenResponse;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@RequiredArgsConstructor
 class JwtProviderTest {
 
     @Autowired
@@ -18,6 +22,17 @@ class JwtProviderTest {
     }
 
     @Test
-    void issueJwtToken() {
+    void issueJwtToken_ShouldGenerateValidTokens() {
+        // given
+        TokenRequest tokenRequest = TokenRequest.builder()
+                .accessToken("test-access-token")
+                .build();
+
+        // when
+        TokenResponse tokenResponse = jwtProvider.issueJwtToken(tokenRequest);
+
+        // then
+        assertNotNull(tokenResponse.accessToken());
+        assertNotNull(tokenResponse.refreshToken());
     }
 }
