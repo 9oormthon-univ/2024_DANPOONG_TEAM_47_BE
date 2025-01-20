ded05e139390011602eea7cf89b34c6ebfc8a295
Jinseong <hdss8234@gmail.com>
2025-01-20 23:06:54 +0900

    feat: SecretKey 의존성 주입
    resolve: #30

---
+++ src/main/java/univ/goormthon/kongju/global/config/KeyConfig.java
@@ -0,0 +1,16 @@
+package univ.goormthon.kongju.global.config;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+public class KeyConfig {
+
+    @Bean
+    public SecretKey secretKey() {
+        return Jwts.SIG.HS256.key().build();
+    }
+}
