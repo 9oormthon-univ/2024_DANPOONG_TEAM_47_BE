5e40744ce6ddb45e6b9ae4b68f503e7030a193f5
Jinseong <hdss8234@gmail.com>
2025-01-29 00:44:15 +0900

    feat: STOMP 이용 WebSocket 서버 구성
    resolve: #33

--- src/main/java/univ/goormthon/kongju/global/config/WebSocketConfig.java
+++ src/main/java/univ/goormthon/kongju/global/config/WebSocketConfig.java
@@ -2,20 +2,30 @@ package univ.goormthon.kongju.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import univ.goormthon.kongju.domain.chat.handler.CustomWebSocketHandler;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler(), "/ws/chat")
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")
                 .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // destination header가 /app으로 시작하는 메시지는 @MessageMapping으로 라우팅
+        registry.setApplicationDestinationPrefixes("/app");
+
+        // destination header가 /topic으로 시작하는 메시지는 in-memory message broker로 라우팅
+        registry.enableSimpleBroker("/topic");
     }
 
     @Bean
